80511cfdfd2540f87186781b42968abf4d08d469
Mike Rugh <mike.rugh@gmail.com>
2022-07-17 20:29:58 -0700

    finishes js functionality, cleans up code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,27 @@
-var calendarObj = {date:"" , time:"" , text:""};
+//set variables
 var today = moment().format("[Today is ] dddd, MMMM Do YYYY, h:mm");
 var textAreaEl = document.getElementsByClassName("text-area");
 var now = moment().format("HH");
 var saveEl = document.getElementsByClassName("saveBtn");
+var calendarItems = [];
+var timeEl = document.querySelectorAll('tr');
+var stripped = [];
 
+//get today
 $("#currentDay").text(today);
+
+//creates reference array for time based on calendar row time headers
+function helper() {
+    for (n = 0; n < timeEl.length; n++){
+        stripped.push(timeEl[n].children[0].textContent.replace(/[\n\r]+|[\s]{2,}/g, ' ').trim());
+    }
+    return stripped;
+}
 
 function timeSlide() {
     var timeEl = document.querySelectorAll('tr');
-    // console.log(timeEl);
     for (i = 0; i < timeEl.length; i++){
         var convertTime = moment(timeEl[i].children[0].textContent, 'ha').format('HH');
-        // console.log("timeEl child: ", timeEl[i].children[0]);
-        // console.log("convertTime: ", convertTime);
         if (convertTime < now) {
             $(timeEl[i].children[1]).addClass("past");
         } else if (convertTime > now) {
@@ -23,81 +32,103 @@ function timeSlide() {
     }
 }
 
+//saves text input
 function saveText(event) {
-    var dayObj = {date:"", item:""};
-    // console.log("parent: ", $(this).parent().children()[1].children[0].value);
+    var calendarObj = {date:"", item:[]};
+    var itemObj = {time:"" , text:""};
+    var todayDate = moment().format('DD-MM-YYYY');
+    var timeDay = $(this).parent().children()[0].textContent;
     var newText = $(this).parent().children()[1].children[1].value;
+
+    itemObj.time = timeDay;
+    itemObj.text = newText;
+
+    var localCheck = JSON.parse(localStorage.getItem("calendar-items"));
+    //checks if there's local data
+    if (localCheck == null){
+        calendarObj.date = todayDate;
+        calendarObj.item.push(itemObj);
+        calendarItems.push(calendarObj);
+    } else if (localCheck.length > 0) {
+        calendarItems = JSON.parse(localStorage.getItem("calendar-items"));
+        for (i = 0; i < localCheck.length; i++){
+            //checks if calendarItems already has an object for that date
+            if (calendarItems[i].date === todayDate){
+                for (x = 0; x < calendarItems[i].item.length; x++){
+                    if (calendarItems[i].item[x].time === itemObj.time) {
+                        calendarItems[i].item[x].text = itemObj.text;
+                    } 
+                }
+            }
+        }
+    }
+    localStorage.setItem("calendar-items", JSON.stringify(calendarItems));
     
     $(this).parent().children()[1].children[1].remove();
     $(this).parent().children()[1].children[0].remove();
     $(this).parent().children()[1].textContent = newText;
-
-    // var newText = $parentEl.children[1].children[0].value;
-    console.log("newText: ", newText);
 }
 
-$(saveEl).on('click', saveText);
-
-timeSlide();
-
-console.log(textAreaEl);
-
-
-
-
+//listener for text input elements of calendar
 $(document).on('click', '.text-area', function() {
     var classList = $(this).attr("class");
-    console.log(classList);
-    var $textInput = $('<textarea class="form-control"' + classList + '></textarea>');
-    var $form = $('<td class="text-area form-group past"></td>');
-        // $input   = $('<input type="text">');
-
-    // Place the original element inside a wrapper:
-    // $(this).before($input);
+    var $textInput = $('<textarea class="form-control ' + classList + '"></textarea>');
+    var $form = $('<td class="form-group ' + classList + '"></td>');
+        ;
     $(this).before($form);
     $(this).remove().appendTo($form).hide();
-    // $(this).text.remove();
-
-    // $(this).append($textInput);
-    // $(this).hide();
-
+    
     // Add some styles:
     $textInput.css({
         display: 'block',
         width: '100%',
         height: '100%',
         font:  $(this).css('font'),
-        color: 'black'
+        color: 'black',
     });
     $textInput.css("background-color", "transparent");
+    $textInput.css("border", "none");
 
     // Build up the form:
     $form.append($textInput);
-    // $(this).append($input);
     $textInput.val($(this).text()).focus();
-    console.log("focus: ", $textInput.val($(this).text()));
 });
 
-$(".text-area").change(function(){
-    console.log("change called");
-})
+//initializes data on page load/reload
+function init() {
+    // helper();
+    var calendarObj = {date:"", item:[]};
+    var localCheck = JSON.parse(localStorage.getItem("calendar-items"));
+    var todayDate = moment().format('DD-MM-YYYY');
+    //if no calendar object, create one for today
+    if (localCheck === null){
+        for (z = 0; z < stripped.length; z++){
+            var itemObj = {time:"" , text:""};
+            itemObj.time = stripped[z];
+            calendarObj.item.push(itemObj);
+            calendarObj.date = todayDate;
+        }
+        calendarItems.push(calendarObj)
+        localStorage.setItem("calendar-items", JSON.stringify(calendarItems));
+    } else { //calendar object found
+        for (i = 0; i < localCheck.length; i++){
+            if (localCheck[i].date === todayDate){
+                calendarItems = localCheck[i];
+            }
+        }
+        //finds and appends stored data for relevant date and time
+        for (i = 0; i < calendarItems.item.length; i++){
+            for (n = 0; n < stripped.length; n++){
+                if (calendarItems.item[i].time === stripped[n]){
+                    $(timeEl[n]).children()[1].append(calendarItems.item[i].text + " ");
+                }
+            }
+        }
+    }
+}
 
-$(document).on('submit', '.form-control', function(e) {
-    // Don't actually submit the form:
-    console.log("submit called");
-    e.preventDefault();
-
-    var val       = $(this).find('input').val();
-        $wrapper  = $(this).parent(),
-        $original = $wrapper.children().first();
-        // $original = $(this).children.first();
-
-    // Use text() instead of html() to prevent unwanted effects.
-    // $original.text(val);
-    $wrapper.text(val)
-    $original.remove();
-    // $wrapper.after($original);
-    // $original.show();
-    // $wrapper.remove();   
-});
-
+//Activate save buttons, run time styling, initialize page data
+$(saveEl).on('click', saveText);
+timeSlide();
+helper();
+init();
